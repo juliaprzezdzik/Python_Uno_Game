7e3a75f9ed67fa296abb0da69a247449207d6528
Helen <122322678+ziraaell@users.noreply.github.com>
2024-12-08 10:37:29 +0100

    Update Deck.py

--- src/Deck.py
+++ src/Deck.py
@@ -10,7 +10,7 @@ class Deck:
     def __init__(self):
         self.colors = ['Red', 'Green', 'Blue', 'Yellow', 'All']  
         self.numbers = [str(value) for value in range (1,10)]
-        self.special_cards = ['Skip', 'Reverse', 'Draw Two']
+        self.special_cards = ['Skip', 'Draw Two']
         self.wild_cards = ['Wild', 'Wild Draw Four']
         self.deck = []
         self.discarded = []
@@ -21,11 +21,11 @@ class Deck:
             self.deck.append(Card('0', color))
 
             for number in self.numbers:
-                for i in range (2):
+                for _ in range (2):
                     self.deck.append(Card(number, color))
             
             for special_card in self.special_cards:
-                for i in range (2):
+                for _ in range (2):
                     self.deck.append(Card(special_card, color))
 
         for wild_card in self.wild_cards:
@@ -56,11 +56,18 @@ class Deck:
             self.discard_card(temp)
             self.shuffle()
         return self.deck.pop()
- 
+    
+    def get_first_card(self):
+        first_card = self.deck.pop()
+        while first_card.value in (self.wild_cards + self.special_cards):
+            first_card = self.deck.pop()
+            self.discard_card(first_card)
+        return first_card
+
     def display_deck(self):
         for card in self.deck:
             print(f'{card}')
 
     def display_discarted(self):
         for card in self.discarded:
-            print(f'{card}')
+            print(f'{card}')      
