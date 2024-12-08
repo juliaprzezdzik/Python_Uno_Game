17fd95d76ab5fdb2ff91e787a51fa2bc02d97da7
Helen <122322678+ziraaell@users.noreply.github.com>
2024-12-08 16:01:10 +0100

    Update GameInterface.py

--- src/GameInterface.py
+++ src/GameInterface.py
@@ -25,12 +25,17 @@ FONT = pygame.font.SysFont("Arial", 20)
 CARD_WIDTH, CARD_HEIGHT = 80, 120
 CARD_SPACING = 10
 
-def draw_card(card, x, y, highlight=False):
-    color = COLORS[card.color]
+def draw_card(card, x, y, highlight=False, draw_pile = False):
+    if draw_pile:
+        color = (0,0,0)
+        text = FONT.render("UNO", True, WHITE)
+    else:
+        color = COLORS[card.color]
+        text = FONT.render(card.value, True, BLACK)    
+
     pygame.draw.rect(screen, color, (x, y, CARD_WIDTH, CARD_HEIGHT))
     if highlight:
         pygame.draw.rect(screen, HIGHLIGHT, (x, y, CARD_WIDTH, CARD_HEIGHT), 5)
-    text = FONT.render(card.value, True, BLACK)
     text_rect = text.get_rect(center=(x + CARD_WIDTH // 2, y + CARD_HEIGHT // 2))
     screen.blit(text, text_rect)
 
@@ -51,67 +56,58 @@ def display_player_hand(y, player):
         x += CARD_WIDTH + CARD_SPACING
     return highlighted_card
 
-def draw_deck_cards(x, y, highlight=False):
-    color = (0,0,0)
-    pygame.draw.rect(screen, color, (x, y, CARD_WIDTH, CARD_HEIGHT))
-    if highlight:
-        pygame.draw.rect(screen, HIGHLIGHT, (x, y, CARD_WIDTH, CARD_HEIGHT), 5)
-    text = FONT.render("UNO", True, WHITE)
-    text_rect = text.get_rect(center=(x + CARD_WIDTH // 2, y + CARD_HEIGHT // 2))
-    screen.blit(text, text_rect)
-
-def display_deck(deck):
+def display_and_check_deck(deck):
     x = (WIDTH - CARD_WIDTH)// 2
     y = (HEIGHT - CARD_HEIGHT - 20) // 2
     mouse_pos = pygame.mouse.get_pos()
-
     card_rect = pygame.Rect(x - CARD_WIDTH - 4 * CARD_SPACING, y, CARD_WIDTH, CARD_HEIGHT)
     highlight = card_rect.collidepoint(mouse_pos)
-    draw_deck_cards(x - CARD_WIDTH - 4 * CARD_SPACING, y, highlight)
-
+    draw_card(None, x - CARD_WIDTH - 4 * CARD_SPACING, y, highlight, True)
     first_card = deck.get_top_discarded_card()
     draw_card(first_card, x, y)
-
     return highlight
 
 def run():
     game = Game()
     game.start_game()
-
     selected_card = None
-    running = True
+    is_running = True
+    pause_after_card = False
 
-    while game.check_winner() == None and running:
+    while game.check_winner() == None and is_running:
         screen.fill(WHITE)
         display_player_hand(20, game.players[1])
         highlighted_card = display_player_hand(HEIGHT - CARD_HEIGHT - 20, game.players[0])
+        is_draw_pile_clicked = display_and_check_deck(game.deck) 
 
-        if game.count_turn == 0:   
-            uno_card = display_deck(game.deck)       
+        if game.count_turn == 0:
+            if pause_after_card:
+                pygame.display.flip()
+                pygame.time.wait(1000)
+                pause_after_card = False  
+                game.track_turn() 
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    is_running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     if highlighted_card is not None:
                         selected_card = highlighted_card
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE and selected_card is not None:
-                        if game.play_card(0, selected_card):
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        if selected_card is not None and game.play_card(0, selected_card):
                             selected_card = None
+                            pause_after_card = True
+                        if is_draw_pile_clicked:
+                            game.draw_card(0)
                             game.track_turn()
-                    if event.key == pygame.K_SPACE and uno_card is True:
-                        game.draw_card(0)
-                        game.track_turn()
-
             if selected_card is not None:
                 x = selected_card * (CARD_WIDTH + CARD_SPACING) + CARD_SPACING
                 y = HEIGHT - CARD_HEIGHT - 20
-                pygame.draw.rect(screen, HIGHLIGHT, (x, y, CARD_WIDTH, CARD_HEIGHT), 5)
-            
-        elif game.count_turn == 1:
+                pygame.draw.rect(screen, HIGHLIGHT, (x, y, CARD_WIDTH, CARD_HEIGHT), 5)        
+        else:
             game.random_move()
             game.track_turn()
-
         pygame.display.flip()
 
     pygame.quit()
