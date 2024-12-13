ac615b1e4e12471e0dbb2a06e4cb3f3d6d0d957e
Helena Jońca <122322678+ziraaell@users.noreply.github.com>
2024-12-13 16:41:11 +0100

    Update GameInterface.py

--- src/GameInterface.py
+++ src/GameInterface.py
@@ -23,6 +23,8 @@ COLORS = {
     "Yellow": (255, 255, 0),
     "All": (255, 255, 255),
 }
+CP_WIDTH = 300
+CP_HEIGHT = 300
 FONT = pygame.font.SysFont("Arial", 20)
 
 def display_start_menu():
@@ -48,7 +50,7 @@ def start_menu():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if start_button_rect.collidepoint(event.pos):
                     running = False
-                    
+
 def draw_card(card, x, y, disable_highlight, is_mouse_over=False, draw_pile=False):
     if draw_pile:
         color = (0, 0, 0)
@@ -102,19 +104,19 @@ def display_and_check_deck(deck, disable_highlight=False):
     draw_card(first_card, x, y, disable_highlight)
     return is_mouse_over
 
-def draw_player_avatar(player_num, is_turn, y_position):
+def draw_player_avatar(backround2, player_num, y_position):
+    background = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
     if player_num == 1:
         avatar_image = pygame.image.load('../assets/avatar1.jpg')
     else:
         avatar_image = pygame.image.load('../assets/avatar2.jpg')
     avatar_image = pygame.transform.scale(avatar_image, (100, 100))
     avatar_rect = pygame.Rect(WIDTH - 150, y_position, 100, 100)
-    screen.blit(avatar_image, avatar_rect)
-    if is_turn:
-        pygame.draw.rect(screen, HIGHLIGHT, avatar_rect, 5)
+    background.blit(avatar_image, avatar_rect)
     player_text = FONT.render(f"Player {player_num + 1}", True, WHITE)
     text_rect = player_text.get_rect(center=(WIDTH - 100, y_position + 110))
-    screen.blit(player_text, text_rect)
+    background.blit(player_text, text_rect)
+    return background
 
 def display_victory_screen():
     screen.fill((0, 128, 0))
@@ -145,22 +147,65 @@ def wait_for_exit():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
                 waiting = False
 
+def draw_color(color, x, y, disable_highlight = False):
+    mouse_pos = pygame.mouse.get_pos()
+    color_rect = pygame.Rect(x, y, CP_WIDTH//2, CP_HEIGHT//2)
+    is_mouse_over = color_rect.collidepoint(mouse_pos)
+    pygame.draw.rect(screen, color, color_rect, border_radius=10)
+    if is_mouse_over:
+        pygame.draw.rect(screen, HIGHLIGHT, color_rect, 5, border_radius=10)
+    return is_mouse_over
+
+def draw_player_turn(count_turn):
+    if count_turn == 0:
+        avatar_rect = pygame.Rect(WIDTH - 150, HEIGHT - 120, 100, 100)
+    else:
+        avatar_rect = pygame.Rect(WIDTH - 150, 20, 100, 100)
+    pygame.draw.rect(screen, HIGHLIGHT, avatar_rect, 5)
+
+def display_color_picker():
+    mid_x = WIDTH // 2
+    mid_y = HEIGHT // 2
+    color = None
+    picker_rect = pygame.Rect(mid_x - CP_WIDTH//2 - 15 , mid_y - CP_HEIGHT//2 - 15, CP_WIDTH + 30, CP_HEIGHT + 30)
+    pygame.draw.rect(screen, BLACK, picker_rect, border_radius=15)  
+    red = draw_color(COLORS["Red"], mid_x - CP_WIDTH//2 - 5, mid_y - CP_HEIGHT//2 - 5)
+    blue = draw_color(COLORS["Blue"], mid_x + 5, mid_y - CP_HEIGHT//2 - 5)
+    yellow = draw_color(COLORS["Yellow"], mid_x - CP_WIDTH//2 - 5, mid_y + 5)
+    green = draw_color(COLORS["Green"], mid_x + 5, mid_y + 5)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if red:
+                color = "Red"
+            if blue:
+                color = "Blue"
+            if yellow:
+                color = "Yellow"
+            if green:
+                color = "Green"
+    return color
 
 def run():
+    start_menu()
     game = Game()
-    game.start_game(initial_hand_size=7)
+    game.start_game(initial_hand_size=3)
     selected_card = None
     is_running = True
     pause_after_card = False
+    color = None
+    background_surface = pygame.Surface((WIDTH, HEIGHT))
+    background_surface.blit(pygame.transform.scale(background_image, (WIDTH, HEIGHT)), (0, 0))
+    avatar1 = draw_player_avatar(background_surface, 1, 20)
+    avatar2 = draw_player_avatar(background_surface, 0, HEIGHT - 120)
     while game.check_winner() is None and is_running:
-        global WIDTH, HEIGHT
-        WIDTH, HEIGHT = screen.get_size()
-        screen.fill(WHITE)
-        background = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
-        screen.blit(background, (0, 0))
-        draw_player_avatar(1, game.count_turn == 1, 20)
+        screen.blit(background_surface, (0, 0))
+        screen.blit(avatar1, (0, 0)) 
+        screen.blit(avatar2, (0, 0))
+        draw_player_turn(game.count_turn)       
         display_player_hand(20, game.players[1], is_visible=False)
-        draw_player_avatar(0, game.count_turn == 0, HEIGHT - 120)
         highlighted_card = display_player_hand(HEIGHT - CARD_HEIGHT - 20, game.players[0])
         is_draw_pile_clicked = display_and_check_deck(game.deck)
         if pause_after_card:
@@ -185,11 +230,19 @@ def run():
                         if is_draw_pile_clicked:
                             game.draw_card(0)
                             pause_after_card = True
+                        if game.is_color_changed:
+                            pause_after_card = False
+                            while color is None:
+                                color = display_color_picker()
+                                pygame.display.flip()
+                            game.change_color(color)
+                            pause_after_card = True
+                            color = None
         else:
             game.random_move()
             pause_after_card = True
         pygame.display.flip()
-    if game.check_winner == game.players[0]:
+    if game.check_winner() == game.players[0]:
         display_victory_screen()
         pygame.quit()
     else:
@@ -199,3 +252,4 @@ def run():
 if __name__ == "__main__":
     start_menu()
     run()
+
