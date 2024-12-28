5b380f95408d66dd2ddd6c04716794f0afaa30f7
Maria <mariaagnieszkazi@gmail.com>
2024-12-28 17:30:56 +0100

    Integrated ReplayBuffer with Agent. Added size method

--- src/Agent.py
+++ src/Agent.py
@@ -1,6 +1,7 @@
 import torch as T
 import numpy as np
 from Model import DQN
+from ReplayBuffer import CustomReplayBuffer
 
 class Agent():
     def __init__(self, gamma, epsilon, learning_rate, input_dims, batch_size, 
@@ -13,22 +14,11 @@ class Agent():
         self.action_space = [i for i in range(n_actions)]
         self.mem_size = max_mem_size 
         self.batch_size = batch_size
-        self.memory_counter = 0
         self.Q_eval = DQN(self.learning_rate, n_actions=n_actions, input_size=input_dims, layer1_size=256, layer2_size=256)
-        self.state_memory = np.zeros((self.mem_size, input_dims), dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, input_dims), dtype=np.float32)
-        self.action_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool_)
+        self.replay_buffer = CustomReplayBuffer(max_size=max_mem_size, state_dim=input_dims, n_actions=n_actions)
 
     def store_transition(self, state, action, reward, new_stage, done):
-        index = self.memory_counter % self.mem_size 
-        self.state_memory[index] = state
-        self.new_state_memory[index] = new_stage
-        self.reward_memory[index] = reward
-        self.action_memory[index] = action
-        self.terminal_memory[index] = done
-        self.memory_counter += 1
+        self.replay_buffer.store_transition(state, action, reward, new_stage, done)
 
     def choose_action(self, state, available_actions):
         state = T.tensor(state, dtype=T.float32).to(self.Q_eval.device)
@@ -44,24 +34,19 @@ class Agent():
         return action_idx
 
     def learn(self):
-        if self.memory_counter < self.batch_size:
-            return 
-        self.Q_eval.optimizer.zero_grad()
-        max_mem = min(self.memory_counter, self.mem_size)
-        batch = np.random.choice(max_mem, self.batch_size, replace=False)
-        batch_index = np.arange(self.batch_size, dtype=np.int32)
-        state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
-        new_state_batch = T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
-        reward_batch = T.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
-        terminal_batch = T.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
-        action_batch = T.tensor(self.action_memory[batch], dtype=T.long).to(self.Q_eval.device)
-        q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
-        q_next = self.Q_eval.forward(new_state_batch)
-        q_next[terminal_batch] = 0.0
-        q_target = reward_batch + self.gamma * T.max(q_next, dim=1)[0]
+        if self.replay_buffer.size() < self.batch_size:
+            return
+        samples = self.replay_buffer.sample_buffer(self.batch_size)
+        states = samples["state"]
+        actions = samples["action"].long()
+        rewards = samples["reward"]
+        next_states = samples["next_state"]
+        dones = samples["done"]
+        q_eval = self.Q_eval.forward(states)[range(self.batch_size), actions]
+        q_next = self.Q_eval.forward(next_states).max(dim=1)[0]
+        q_next[dones] = 0.0
+        q_target = rewards + self.gamma * q_next
         loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
         loss.backward()
         self.Q_eval.optimizer.step()
-        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min else self.eps_min
-
-
+        self.epsilon = max(self.eps_min, self.epsilon - self.eps_dec)
--- src/ReplayBuffer.py
+++ src/ReplayBuffer.py
@@ -1,39 +1,28 @@
-# !! wstepna wersja ReplayBuffer - jeszcze nie zintegrowana z projektem !!!!
-
 import torch
 from torchrl.data import ReplayBuffer, ListStorage
 from tensordict import TensorDict
 
 class CustomReplayBuffer:
     def __init__(self, max_size, state_dim, n_actions):
-        """
-        max_size - maksymalny rozmiar bufora
-        state_dim - wymiary stanu (ile informacji opisuje stan w grze)
-        n_actions - liczba możliwych akcji
-        """
-        self.storage = ListStorage(max_size=max_size)  # lista do przechowywania dsowiadczen
+        self.storage = ListStorage(max_size=max_size)
         self.buffer = ReplayBuffer(storage=self.storage)
         self.state_dim = state_dim
         self.n_actions = n_actions
 
     def store_transition(self, state, action, reward, next_state, done):
-        #TensorDict przechowuje dane jednego kroku agenta
         transition = TensorDict({
-            "state": torch.tensor(state, dtype=torch.float32), #aktualny stan gry
-            "action": torch.tensor(action, dtype=torch.float32), #akcja, ktora agent wykonal
-            "reward": torch.tensor(reward, dtype=torch.float32), # nagroda za akcje
-            "next_state": torch.tensor(next_state, dtype=torch.float32), #stan po akjcji
-            "done": torch.tensor(done, dtype=torch.bool), #czy gra sie skonczyla
+            "state": torch.tensor(state, dtype=torch.float32),
+            "action": torch.tensor(action, dtype=torch.float32),
+            "reward": torch.tensor(reward, dtype=torch.float32),
+            "next_state": torch.tensor(next_state, dtype=torch.float32),
+            "done": torch.tensor(done, dtype=torch.bool),
         }, batch_size=[])
 
-        # dodawanie doświadczenie do bufora
         self.buffer.add(transition)
 
     def sample_buffer(self, batch_size):
-        """
-        losowe probkowanie z bufora
-        batch_size - liczba próbek do pobrania
-        zwraca losowe próbki w formie TensorDict
-        """
         samples = self.buffer.sample(batch_size=batch_size)
         return samples
+    
+    def size(self):
+        return len(self.storage)
