3fb058f764dd1e911603f6963a1eee5e66261350
marywinter3 <116124143+marywinter3@users.noreply.github.com>
2024-12-29 21:01:04 +0100

    Update Action.py

--- src/Action.py
+++ src/Action.py
@@ -1,5 +1,6 @@
 from src.GameState import GameState
 
+
 class Action:
     def __init__(self, state):
         self.game_state = state
@@ -8,38 +9,49 @@ class Action:
     def permorm_action(self, game, action, state):
         reward = 0
         prev_state = state
+        has_valid_move = any(
+            game.is_valid_move(game.players[1].cards_in_hand[i], game.deck.get_top_discarded_card())
+            for i in range(game.players[1].count_cards_in_hand())
+        )
+
         if action >= game.players[1].count_cards_in_hand():
             game.draw_card(1)
-            reward = -1
-        elif game.play_card(1, action):
+            if has_valid_move:
+                reward = -2
+            return state, reward, False
+        if game.play_card(1, action):
             card_played = game.deck.get_top_discarded_card()
-            if card_played.value == "Draw Two":
-                reward += 2
-            elif card_played.value == "Wild Draw Four":
-                reward += 4
-            elif card_played.value == "Skip":
-                reward += 1
-            elif card_played.value == "Wild":
-                reward += 3
+            if card_played.value == "Skip":
+                reward += 5
             if game.is_color_changed:
                 game.change_color(state.most_common_color)
-            reward = +10
+                reward += 4
         else:
-            reward = -10
+            reward -= 1
+            card_played = None
         if game.check_winner() == game.players[1]:
-
-            reward += 10
+            reward += 50
         elif game.check_winner() == game.players[0]:
             reward -= 10
         else:
             game.track_turn()
             game.random_move(0)
+        total_cards_in_hand = game.players[1].count_cards_in_hand()
+        opponent_cards = game.players[0].count_cards_in_hand()
+        if card_played and opponent_cards == 1:
+            reward += 3 if card_played.value in ["Draw Two", "Wild Draw Four"] else -1
+        elif card_played and opponent_cards <= 2:
+            reward += 2 if card_played.value in ["Draw Two", "Wild Draw Four"] else 0
+        if total_cards_in_hand == 1:
+            reward += 3
+        reward += max(0, 5 - total_cards_in_hand)
         next_game_state = GameState(game)
         next_state = next_game_state
         action_effectiveness = self.calculate_action_effectiveness(prev_state, next_state)
         reward += action_effectiveness
-        return reward
-    
+        done = total_cards_in_hand == 0 or opponent_cards == 0
+        return next_state, reward, done
+
     def calculate_action_effectiveness(self, prev_state, next_state):
         prev_score = self.calculate_state_score(prev_state)
         next_score = self.calculate_state_score(next_state)
@@ -50,7 +62,7 @@ class Action:
             return -1
         else:
             return 0
-        
+
     def calculate_state_score(self, state):
         player_cards_count = state.player_cards_count
         opponent_cards_count = state.opponent_cards_count
