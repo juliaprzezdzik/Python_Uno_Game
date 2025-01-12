942daca14468c20bc1a11b5ac66a12577366830d
juliaprzezdzik <113039967+juliaprzezdzik@users.noreply.github.com>
2025-01-12 18:29:36 +0100

    Removed unnecessary elements

--- src/ReplayBuffer.py
+++ src/ReplayBuffer.py
@@ -1,10 +1,7 @@
 import torch
 from torchrl.data import ReplayBuffer, ListStorage
 from tensordict import TensorDict
-import matplotlib.pyplot as plt
-import numpy as np
 import random
-from scipy.stats import gaussian_kde
 
 class CustomReplayBuffer:
     def __init__(self, max_size, state_dim, n_actions):
@@ -53,31 +50,3 @@ class CustomReplayBuffer:
     def update_priorities(self, indices, new_priorities):
         for idx, prio in zip(indices, new_priorities):
             self.storage[idx]["priority"] = torch.tensor(prio, dtype=torch.float32)
-'''
-if __name__ == "__main__":
-    buffer = CustomReplayBuffer(max_size=10000, state_dim=4, n_actions=2)
-    for _ in range(1000):
-        state = np.random.randn(4)
-        action = np.random.randint(0, 2)
-        reward = np.random.randn()
-        next_state = np.random.randn(4)
-        done = np.random.choice([True, False])
-        buffer.store_transition(state, action, reward, next_state, done, priority=1.0)
-    stats = buffer.get_reward_statistics()
-    print(f"Rewards statistics: {stats}")
-    buffer.plot_reward_distribution()
-    buffer.plot_action_distribution()
-    buffer.visualize_rewards_distribution()
-    buffer.visualize_transitions(num_samples=3)
-    batch_size = 5
-    samples_priority = buffer.sample_with_priority(batch_size=batch_size, alpha=0.6)
-    print("Sampled transitions (priority-based): ")
-    for i in range(batch_size):
-        print(f"Transition {i + 1}, Reward = {samples_priority['rewards'][i].item():.2f}, "
-              f"Priority = {buffer.storage[samples_priority['indices'][i]]['priority'].item():.2f}")
-    new_priorities = [abs(samples_priority['rewards'][i].item()) + 0.01 for i in range(batch_size)]
-    buffer.update_priorities(samples_priority['indices'], new_priorities)
-    print("\nUpdated priorities based on absolute reward + 0.01")
-    for i, idx in enumerate(samples_priority['indices']):
-        print(f"Index {idx}, new priority = {buffer.storage[idx]['priority'].item():.2f}")
-'''
