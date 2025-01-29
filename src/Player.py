06fe19115ab817d569257c5bfcdaebc3c69b683e
Helena Jońca <122322678+ziraaell@users.noreply.github.com>
2025-01-29 21:11:26 +0100

    Update Player.py

--- src/Player.py
+++ src/Player.py
@@ -11,6 +11,7 @@ class Player:
         card = self.cards_in_hand[card_index]
         if card:
             deck.discard_card(card)
+            self.last_played_card = card
             self.cards_in_hand.remove(card)
             return card
         else:
@@ -32,6 +33,13 @@ class Player:
     def count_cards_in_hand(self):
         return len(self.cards_in_hand)
     
+    def count_skip_cards(self):
+        colour_count = {"Red" : 0, "Blue" : 0, "Green" : 0, "Yellow" : 0}
+        for card in self.cards_in_hand:
+            if card.value == "Skip":
+                colour_count[card.color] += 1
+        return sum(colour_count.values())
+    
     def count_special_cards(self):
         special_counts = {"Skip" : 0, "Draw Two" : 0, "Wild Draw Four" : 0, "Wild" : 0}
         for card in self.cards_in_hand:
@@ -40,8 +48,12 @@ class Player:
         total = sum(special_counts.values())
         return total
     
+    def count_wild_cards(self):
+        return sum([1 for card in self.cards_in_hand if card.value in ["Wild Draw Four", "Wild"]])
+
+   
     def count_color(self):
-        color_counts = {"Red" : 0, "Blue" : 0, "Green" : 0, "Yellow" : 0, "All" : 0}
+        color_counts = {"Red" : 0, "Blue" : 0, "Green" : 0, "Yellow" : 0}
 
         for card in self.cards_in_hand:
             if card.color in color_counts:
@@ -52,3 +64,15 @@ class Player:
     def get_most_common_color(self):
         color = self.count_color()
         return max(color, key=color.get)
+
+    def count_cards_with_same_value(self):
+        value_counts = {}
+        for card in self.cards_in_hand:
+            if card.value in value_counts:
+                value_counts[card.value] += 1
+            else:
+                value_counts[card.value] = 1
+        return value_counts
+
+    def count_identical_cards(self, card):
+        return sum([1 for card2 in self.cards_in_hand if card2.value == card.value and card2.color == card.color])
