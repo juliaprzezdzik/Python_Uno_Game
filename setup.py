32c780e650692163020e58ee624d025dc143ec41
Helena Jońca <122322678+ziraaell@users.noreply.github.com>
2025-01-29 21:23:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ setup(
 name = "uno_game",
 version ='0.1.0',
 
-packages=find_packages(include=["src", "src.*"]))
-# install_requires=[
-#     'pandas',
-#     'numpy',
-#     'pygame',
-#     'torch',
-#     'tensordict',
-#     'scipy',
-#     ],
+packages=find_packages(include=["src", "src.*", "DQN", "DQN*", "QLearning", "QLearning*"]),
+install_requires=[
+    'pandas',
+    'numpy',
+    'pygame',
+    'torch==2.5.1',
+    'torchrl==0.6.0',
+    'tensordict==0.6.1',
+    'scipy',
+    'matplotlib',
+    ],
+    python_requires='>=3.10',
 )
